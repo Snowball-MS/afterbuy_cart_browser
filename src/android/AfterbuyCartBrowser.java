00b13c57f370c5d69213b376dff2d4b0c95508fb
Snowball <love2415@gmail.com>
2018-05-06 23:03:40 +0900

    Update AfterbuyCartBrowser.java

--- src/android/AfterbuyCartBrowser.java
+++ src/android/AfterbuyCartBrowser.java
@@ -1,1 +1,25 @@
+package com.idve.afterbuy.cartbrowser;
 
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.Config;
+import org.apache.cordova.CordovaArgs;
+import org.apache.cordova.CordovaHttpAuthHandler;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.LOG;
+import org.apache.cordova.PluginManager;
+import org.apache.cordova.PluginResult;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class AfterbuyCartBrowser extends CordovaPlugin {
+  
+    public boolean execute(String action, String url, String evelScript, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("open")) {
+            return true;
+        } else if (action.equals("close")) {
+            return true;
+
+        return false;
+    }
+}
